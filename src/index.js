c9164a444810a912abf619a96bf7c3f45696fef0
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-05-18 07:56:13 +0000

    setup leaderboard

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import './style.css';
 
-console.log('Hello World');
+const scores = [
+    {name: 'Joseph', score: 55}
+];
+
+const scoreList = document.getElementById('scores-list');
+scoreList.innerHTML = scores.map((score) => `<li>${score.name}: ${score.score} </li>`).join('');
