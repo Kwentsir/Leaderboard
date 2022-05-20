5e5b00501733a933a1e501bfeb00b347e25b212f
Joseph Kwesi Kwentsir <kwentsirkwesi@gmail.com>
2022-05-20 07:28:31 +0000

    fix errors

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
-import './style.css';
+import "./style.css";
 
-const baseUrl = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api';
-const scoreList = document.getElementById('scores-list');
-const refreshButton = document.getElementById('refresh');
+const baseUrl =
+  "https://us-central1-js-capstone-backend.cloudfunctions.net/api";
+const scoreList = document.getElementById("scores-list");
+const refreshButton = document.getElementById("refresh");
 
 const getScores = async () => {
   try {
     const response = await fetch(
-      `${baseUrl}/games/qLB7AkzcuaFFiATPmxPW/scores`,
+      `${baseUrl}/games/qLB7AkzcuaFFiATPmxPW/scores`
     );
 
     const data = await response.json();
 
     scoreList.innerHTML = data.result
       .map((score) => `<li>${score.user}: ${score.score}</li>`)
-      .join('');
+      .join("");
   } catch (error) {
-    scoreList.innerHTML = '<li>No scores found</li>';
+    scoreList.innerHTML = "<li>No scores found</li>";
   }
 };
 
@@ -24,27 +25,27 @@ getScores();
 
 const postRecord = async (record) => {
   await fetch(`${baseUrl}/games/qLB7AkzcuaFFiATPmxPW/scores`, {
-    method: 'POST',
+    method: "POST",
 
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
 
     body: JSON.stringify(record),
   });
 };
 
-const addScoreForm = document.getElementById('add-score-form');
+const addScoreForm = document.getElementById("add-score-form");
 
-addScoreForm.addEventListener('submit', (event) => {
+addScoreForm.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const formData = new FormData(event.target);
 
   const record = {
-    user: formData.get('name'),
+    user: formData.get("user"),
 
-    score: formData.get('score'),
+    score: formData.get("score"),
   };
 
   postRecord(record).then(() => {
@@ -54,6 +55,4 @@ addScoreForm.addEventListener('submit', (event) => {
   });
 });
 
-refreshButton.addEventListener('click', () => {
-  getScores();
-});
+refreshButton.addEventListener("click", getScores);
